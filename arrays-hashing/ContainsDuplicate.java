6901b5bc245f298fd87dc341197c13099e39d847
Calebe <calebercarozzi@outlook.com>
2026-05-21 17:19:28 -0300

    feat: add contains-duplicate

---
+++ arrays-hashing/ContainsDuplicate.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class ContainsDuplicate {
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++){
+            if(seen.contains(nums[i])) return true;
+            seen.add(nums[i]);
+        }
+        return false;
+    }
+}
